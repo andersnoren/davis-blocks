02728dc73e9c4d1571b9ecd100ead73beb7d048e
Anders <anders@andersnoren.se>
2023-08-21 10:54:01 +0200

    0.2.1

--- functions.php
+++ functions.php
@@ -5,11 +5,7 @@
 --------------------------------------------------------------------------------------------------- */
 
 function davis_blocks_setup() {
-
-	add_theme_support( 'wp-block-styles' );
-
 	add_editor_style( 'style.css' );
-
 }
 add_action( 'after_setup_theme', 'davis_blocks_setup' );
 
@@ -19,9 +15,7 @@ add_action( 'after_setup_theme', 'davis_blocks_setup' );
 --------------------------------------------------------------------------------------------------- */
 
 function davis_blocks_styles() {
-
 	wp_enqueue_style( 'davis-blocks-styles', get_theme_file_uri( '/style.css' ), array(), wp_get_theme( 'davis-blocks' )->get( 'Version' ) );
-
 }
 add_action( 'wp_enqueue_scripts', 'davis_blocks_styles' );
 
@@ -31,11 +25,9 @@ add_action( 'wp_enqueue_scripts', 'davis_blocks_styles' );
 --------------------------------------------------------------------------------------------------- */
 
 function davis_blocks_register_block_styles() {
-
 	register_block_style( 'core/separator', array(
 		'name'  	=> 'davis-separator',
 		'label' 	=> esc_html__( 'Diamonds', 'davis-blocks' ),
 	) );
-	
 }
 add_action( 'init', 'davis_blocks_register_block_styles' );
--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Davis Blocks ===
 Contributors: Anlino
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=anders%40andersnoren%2ese&lc=US&item_name=Free%20WordPress%20Themes%20from%20Anders%20Noren&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
-Requires at least: 6.1
+Requires at least: 6.2
 Requires PHP: 5.6
-Tested up to: 6.1
+Tested up to: 6.3
 Stable tag: trunk
 License: GPLv2 or later
 License URI: https://gnu.org/licenses/gpl-2.0.html
@@ -44,6 +44,11 @@ Included as part of:
 
 
 == Changelog ==
+
+Version 0.2.1 (2023-08-21)
+-------------------------
+- Removed opt-in of the opinionated wp-block-styles, since it causes issues with pull quote styles in 6.3.
+- Updated styles to account for removal of wp-block-styles.
 
 Version 0.2 (2023-02-08)
 -------------------------
